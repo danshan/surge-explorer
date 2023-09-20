c5bd6cf38d4d955bbf76492251d6581eba895c4f
Honghao SHAN 单弘昊 <honghao.shan@nio.com>
2023-09-20 09:54:58 +0800

    feature init empty project

---
+++ src/main/kotlin/com/shanhh/surge/surgeexporter/SurgeExporterApplication.kt
@@ -0,0 +1,11 @@
+package com.shanhh.surge.surgeexporter
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SurgeExporterApplication
+
+fun main(args: Array<String>) {
+	runApplication<SurgeExporterApplication>(*args)
+}
---
+++ src/test/kotlin/com/shanhh/surge/surgeexporter/SurgeExporterApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.shanhh.surge.surgeexporter
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SurgeExporterApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
